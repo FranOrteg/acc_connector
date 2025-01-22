30f2039049cccb8efde3f982b194f9cd759a0015
FranOrteg <ortegfran@gmail.com>
2025-01-22 10:14:17 +0100

    Items End

--- src/main/java/labit/analytics/Items.java
+++ src/main/java/labit/analytics/Items.java
@@ -93,4 +93,70 @@ public class Items {
             return responseBody;
         }
     }
+
+    /* Returns the custom relationships that are associated with the given item_id */
+    public static String getItemRelationships(String accessToken, String projectId, String itemId) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/items/" + itemId + "/relationships/refs")
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
+            }
+
+            String responseBody = response.body().string();
+            System.out.println("Item Relationships: " + responseBody);
+            System.out.println("");
+            return responseBody;
+        }
+    }
+
+    /* Returns the “tip” version for the given item. */
+    public static String getItemTip(String accessToken, String projectId, String itemId) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/items/:" + itemId + "/tip")
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
+            }
+
+            String responseBody = response.body().string();
+            System.out.println("Item Tip: " + responseBody);
+            System.out.println("");
+            return responseBody;
+        }
+    }
+
+    /* Returns versions for the given item */
+    public static String getItemVersions(String accessToken, String projectId, String itemId) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/items/" + itemId + "/versions")
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
+            }
+
+            String responseBody = response.body().string();
+            System.out.println("Item Versions: " + responseBody);
+            System.out.println("");
+            return responseBody;
+        }
+    }
 }
