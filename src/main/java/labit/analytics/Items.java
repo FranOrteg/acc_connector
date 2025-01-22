1334de4f06568625402d27c457a7dfff46ad0a1c
FranOrteg <ortegfran@gmail.com>
2025-01-22 10:08:34 +0100

    Item continues

--- src/main/java/labit/analytics/Items.java
+++ src/main/java/labit/analytics/Items.java
@@ -6,6 +6,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 public class Items {
 
+    /* Retrieves metadata for a specified item. Items represent word documents, fusion design files, drawings, spreadsheets, etc.*/
     public static String dataItem(String accessToken, String projectId, String itemId) throws IOException {
         OkHttpClient client = new OkHttpClient();
 
@@ -25,5 +26,71 @@ public class Items {
             System.out.println("");
             return responseBody;
         }
-    }{}
+    }
+
+    /* Returns the "parent" folder for the given item. */
+    public static String getItemParentFolder(String accessToken, String projectId, String itemId) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/items/" + itemId + "/parent")
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
+            }
+
+            String responseBody = response.body().string();
+            System.out.println("Item Parent Folder: " + responseBody);
+            System.out.println("");
+            return responseBody;
+        }
+    }
+
+    /* returns the resources (items, folders, and versions) that have a custom relationship with the given item_id */
+    public static String getItemRefs(String accessToken, String projectId, String itemId) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/items/" + itemId + "/refs")
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
+            }
+
+            String responseBody = response.body().string();
+            System.out.println("Item Refs: " + responseBody);
+            System.out.println("");
+            return responseBody;
+        }
+    }
+
+    /* Returns a collection of links for the given item_id */
+    public static String getItemLinks(String accessToken, String projectId, String itemId) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/items/" + itemId + "/relationships/links")
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
+            }
+
+            String responseBody = response.body().string();
+            System.out.println("Item Links: " + responseBody);
+            System.out.println("");
+            return responseBody;
+        }
+    }
 }
