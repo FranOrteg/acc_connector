46371fdd8a02fe61a7fa49e93e2f5b76b403d1b2
FranOrteg <ortegfran@gmail.com>
2025-02-21 10:11:11 +0100

    Returns a set of already available downloads for this version.

--- src/main/java/labit/analytics/Versions.java
+++ src/main/java/labit/analytics/Versions.java
@@ -2,7 +2,9 @@ package labit.analytics;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -37,34 +39,77 @@ public class Versions {
 
      /* DEVUELVE UNA COLECCIÓN DE FORMATOS DE ARCHIVOS A LOS QUE ESTA VERSIÓN
        PODRÍA CONVERTIRSE Y DESCARGARSE */
-       public static List<String> getFileFormatCollection(String accessToken, String projectId, String versionId) throws IOException {
+    public static List<String> getFileFormatCollection(String accessToken, String projectId, String versionId) throws IOException {
+     OkHttpClient client = new OkHttpClient();
+    
+     Request request = new Request.Builder()
+             .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/versions/" + versionId + "/downloadFormats")
+             .addHeader("Authorization", "Bearer " + accessToken)
+             .addHeader("Content-Type", "application/json")
+             .build();
+    
+             try (Response response = client.newCall(request).execute()) {
+                 if (!response.isSuccessful()) {
+                     throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
+                }
+         
+                String responseBody = response.body().string();
+         
+                JSONObject json = new JSONObject(responseBody);
+         
+                // Extraer la colección de formatos de la respuesta
+         
+                List<String> fileFormats = new ArrayList<>();
+         
+                JSONArray formatsArray = json.optJSONArray("formats"); // Ajusta el nombre si es diferente en la API
+         
+                if (formatsArray != null) {
+         
+                for (int i = 0; i < formatsArray.length(); i++) {
+                    fileFormats.add(formatsArray.getString(i));     
+                }
+         
+            }
+            return fileFormats;
+        }
+    }
+
+    /* DEVUELVE UN SET DE LAS DESCARGAS DISPONIBLES PARA ESTA VERSION */
+    public static Set<String> getAvaliableDownloads(String accessToken, String projectId, String versionId) throws IOException {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/versions/" + versionId + "/downloadFormats")
-                .addHeader("Authorization", "Bearer " + accessToken)
-                .addHeader("Content-Type", "application/json")
-                .build();
+             .url("https://developer.api.autodesk.com/data/v1/projects/" + projectId + "/versions/" + versionId + "/downloads")
+             .addHeader("Authorization", "Bearer " + accessToken)
+             .addHeader("Content-Type", "application/json")
+             .build();
 
-        try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
-            }
-
-            String responseBody = response.body().string();
-            JSONObject json = new JSONObject(responseBody);
-
-            // Extraer la colección de formatos de la respuesta
-            List<String> fileFormats = new ArrayList<>();
-            JSONArray formatsArray = json.optJSONArray("formats"); // Ajusta el nombre si es diferente en la API
-
-            if (formatsArray != null) {
-                for (int i = 0; i < formatsArray.length(); i++) {
-                    fileFormats.add(formatsArray.getString(i));
+             try (Response response = client.newCall(request).execute()) {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response + ", body: " + response.body().string());
                 }
-            }
 
-            return fileFormats;
-        }
+                String responseBody = response.body().string();
+
+                JSONObject json = new JSONObject(responseBody);
+
+                // Extraer el del set los resultados disponibles
+
+                Set<String> downloadsSet = new HashSet<>();
+
+                if(json.has("data") && json.get("data") instanceof JSONArray){
+                    JSONArray downloadsArray = json.getJSONArray("data");
+
+                    for (int i = 0; i < downloadsArray.length(); i++){
+                        JSONObject downloadObj = downloadsArray.getJSONObject(i);
+                        
+                        if (downloadObj.has("id")) {
+                            downloadsSet.add(downloadObj.getString("id"));
+                        }
+                    }
+                }
+
+                return downloadsSet;
+        }    
     }
 }
